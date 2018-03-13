de0ca5eea83ccb18078d76db67f532570e311b8c
sherrodlib <37336701+sherrodlib@users.noreply.github.com>
2018-03-13 09:26:01 -0400

    Update zotero_sherpa.php
    Now accepts collection types (user or group). Uses updated ZendHTTP via Composer

--- zotero_sherpa.php
+++ zotero_sherpa.php
@@ -1,28 +1,52 @@
 <?php
-//set_include_path(get_include_path() . PATH_SEPARATOR . '/path/to/Zend/library');
-require_once('Zend/Http/Client.php');
+
+require_once 'vendor/autoload.php';
 require_once('SherpaRomeo.php');
 
-if ((! isset($argv[1])) || (! isset($argv[2]))){
-  echo "Not enough parameters\n";
-  usage();
-  exit;
+if (( isset($argv[1])) && ( isset($argv[2]))){
+  $userid = $argv[1];
+  $zotero_key = $argv[2];
+  $sherpa_romeo_key = $argv[3];
+  $collection_type = $argv[4];
+  $limit = $argv[5];
+
+}
+else if (isset($_GET['userid'])){
+    $userid = $_GET['userid'];
+    $zotero_key = $_GET['zotero_key'];
+    $sherpa_romeo_key = $_GET['sherpa_romeo_key'];
+    $collection_type = $_GET['collection_type']; // 'group' or 'user'
+    $limit = $_GET['collection_type']; // limit to most recent
+  }
+else {
+    echo "Not enough parameters\n";
+    usage();
+    exit; 
 }
 
-$userid = $argv[1];
-$zotero_key = $argv[2];
-$sherpa_romeo_key = $argv[3];
-
-$url_base = 'https://api.zotero.org/users/'.$userid.'/';
-$client = new Zend_HTTP_Client();
-$client->setUri($url_base.'items/?key='.$zotero_key.'&itemType=journalArticle&content=json');
-$response = $client->request();
-
-if (200 != $response->getStatus()){
-  echo "Failed to get items (".$response->getStatus().")\n";
-  exit;
+$type ='';
+if ($collection_type == 'group'){
+  $type ='groups';
+}
+else{
+  $type ='users';
 }
 
+$url_base = 'https://api.zotero.org/'.$type.'/'.$userid.'/';
+//$client = new Zend_HTTP_Client();
+$client = new Zend\Http\Client();
+
+$client->setUri($url_base.'items/?key='.$zotero_key.'&itemType=journalArticle&content=json&limit='.$limit.'&sort=dateModified');
+//$response = $client->request();
+$response = $client->send();
+
+//echo $response->getBody();
+if (200 != $response->getStatusCode()){
+  echo "Failed to get items (".$response->getStatusCode().")\n";
+  exit;
+}
+// prints a nice list of updated items like an rss feed
+//echo print_r($response->getBody());
 $items = $response->getBody();
 $items_xml = new DOMDocument();
 $items_xml->loadXML($items);
@@ -39,6 +63,9 @@ foreach ($item_list as $item){
   $item_key = $item->getElementsByTagNameNS('http://zotero.org/ns/api', 'key')->item(0)->nodeValue;
   $issn = $content_json->ISSN;
   $jtitle = $content_json->publicationTitle;
+  
+  //get "extra" content
+  $jextra = $content_json->extra;
  
   if ($issn || $jtitle){
     $sr_key = str_replace(" ", "", $issn.$jtitle);
@@ -51,7 +78,7 @@ foreach ($item_list as $item){
           $sr_responses[$sr_key] = $sr_data;
         }
       }
-      if ($issn_hits == 0){
+     else if ($issn_hits == 0){ // put that else
         if ($jtitle){
           $sr_data = new SherpaRomeo($jtitle, 'jtitle', $sherpa_romeo_key);
           $sr_responses[$sr_key] = $sr_data;
@@ -59,37 +86,51 @@ foreach ($item_list as $item){
       }
     }else{
       $sr_data = $sr_responses[$sr_key];
+      echo "found nothing";
     }
 
     $pubs = array_values($sr_data->getPublishers());
     if (count($pubs) == 1){
       $pub = $pubs[0];
-      $content_json->tags[] = array('tag' => 'SherpaRomeo Pre '.$pub->getPreArchiving(), 'type' => 1);
-      $content_json->tags[] = array('tag' => 'SherpaRomeo Post '.$pub->getPreArchiving(), 'type' => 1); 
-      $content_json->tags[] = array('tag' => 'SherpaRomeo PDF '.$pub->getPdfArchiving(), 'type' => 1); 
+	  
+	  //attempt to set "extra" content
+	  
+	  if (strpos($content_json->extra , 'SherpaRomeo') !== false){
+		//if SherpaRomeo tags already exist in "extra" don't do anything.
+	  }
+	  else{
+		// prepend SherpaRomeo tags to existing content.
+		$content_json->extra = ' #SherpaRomeo Pre '.$pub->getPreArchiving() . '; #SherpaRomeo Post ' 
+		.$pub->getPreArchiving(). '; #SherpaRomeo PDF '.$pub->getPdfArchiving() . '; ' . $content_json->extra ;  
+	  
+		
+	  }
 
       $client->setUri($url_base.'items/'.$item_key.'?key='.$zotero_key);
-      $client->setHeaders('Content-Type', 'application/json');
-      $client->setHeaders('If-Match', $etag);
-      $client->setRawData(json_encode($content_json));
-      $update_response = $client->request('PUT');
+      $client->setHeaders([
+      'Content-Type' => 'application/json',
+      'If-Match' => $etag,
+      ]);
+      $client->setRawBody(json_encode($content_json));
+      $client->setMethod('PUT');
+      $update_response = $client->send();
 
-      if (200 == $update_response->getStatus()){
-        echo "Update successful for ".$item_key."\n";
-      }else{
-        echo "Update failed (".$update_response->getStatus().") for ".$item_key."\n";
+      if (200 == $update_response->getStatusCode()){
+        echo "Nothing to do for ".$item_key."\n";
+      }else if (204 == $update_response->getStatusCode()){
+        echo "Performed an update (".$update_response->getStatusCode().") for ".$item_key. " ".$content_json->publicationTitle."\n";
       }
     }else{
-      echo "Couldn't find single publisher for ".$item_key."\n";
+      echo "Couldn't find single publisher for ".$item_key." ".$content_json->publicationTitle."\n";
     }
   }else{
-    echo "No ISSN or publicationTitle for ".$item_key."\n";
+    echo "No ISSN or publicationTitle for ".$item_key." ".$content_json->publicationTitle."\n";
   }
 }
 
 function usage(){
   echo "\n";
-  echo "zotero_sherpa.pbp [zotero_user] [zotero_key] [sherpa_romeo_key]";
+  echo "zotero_sherpa.pbp [zotero_user] [zotero_key] [sherpa_romeo_key] [collection_type (user/group)] [limit (update most recent)]";
   echo "\n";
 }
 
