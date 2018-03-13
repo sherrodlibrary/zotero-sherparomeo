02c2e6cc7e046251c6411b153fce8710a2cac1cf
sherrodlib <37336701+sherrodlib@users.noreply.github.com>
2018-03-13 09:50:23 -0400

    Create index.php
    A rather clunky way to invoke the import script via a web interface.

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>SHERPA RoMEO - Zotero Import Tool</title> 
+</head>
+<body>
+
+<h1>SHERPA RoMEO -> Zotero Import Tool</h1>
+<p>Import may take over 60 seconds to finish.</p>
+<form action="runscript.php">
+
+
+<input type="submit" value="Run Import Tool">
+</form>
+
+</body>
+
+</html>
